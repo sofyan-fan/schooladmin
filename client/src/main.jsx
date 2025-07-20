e34eb10215ff77c438c3c01ff8b5627dbd32b4ba
Amin <499888@student.fontys.nl>
2025-07-20 04:06:57 +0200

    Added - New shadcn styling

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx';
 import { AuthProvider } from './contexts/AuthProvider.jsx';
-import './index.css';
+import './index_copy.css';
 
 createRoot(document.getElementById('root')).render(
   <Router>
