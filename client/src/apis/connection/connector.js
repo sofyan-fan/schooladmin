03d23a5104971752fc6226d7d359c745344f1f01
Amin <499888@student.fontys.nl>
2025-08-10 10:33:10 +0200

    Added: Refactored Registerpage

--- client/src/apis/connection/connector.js
+++ client/src/apis/connection/connector.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const useJsonServer = import.meta.env.VITE_USE_JSON_SERVER === 'true';
+const useJsonServer = import.meta.env.VITE_USE_JSON_SERVER === 'false';
 
 const connection = axios.create({
   baseURL: useJsonServer ? 'http://localhost:8000' : 'http://localhost:3000',
